f8b6899b3edcca4a6b208b453010e2dbe79a1996
Sasho <sasoh@Macintosh.local>
2015-04-01 11:13:20 +0300

    Removed music files from project

--- GipsyKings/Assets/Scripts/GeneralControlsController.cs
+++ GipsyKings/Assets/Scripts/GeneralControlsController.cs
@@ -8,96 +8,81 @@ public class GeneralControlsController : MonoBehaviour
 	private AudioClip[] musicClips;
 	private AudioSource musicSource;
 
-	public static GeneralControlsController instance
-	{
-		get
-		{
-			if (_instance == null)
-			{
-				_instance = GameObject.FindObjectOfType<GeneralControlsController>();
+	public static GeneralControlsController instance {
+		get {
+			if (_instance == null) {
+				_instance = GameObject.FindObjectOfType<GeneralControlsController> ();
 
 				//Tell unity not to destroy this object when loading a new scene
-				DontDestroyOnLoad(_instance.gameObject);
+				DontDestroyOnLoad (_instance.gameObject);
 			}
 
 			return _instance;
 		}
 	}
 
-	void Awake()
+	void Awake ()
 	{
 
-		musicSource = this.GetComponent<AudioSource>();
-		if (musicSource != null)
-		{
+		musicSource = this.GetComponent<AudioSource> ();
+		if (musicSource != null) {
 			int status = 0;
-			if (PlayerPrefs.HasKey("musicOn") == true)
-			{
-				status = PlayerPrefs.GetInt("musicOn");
+			if (PlayerPrefs.HasKey ("musicOn") == true) {
+				status = PlayerPrefs.GetInt ("musicOn");
 			}
 
-			if (status == 0)
-			{
+			if (status == 0) {
 				musicSource.volume = 0.0f;
-			}
-			else
-			{
-				PlayMusic();
+			} else {
+				PlayMusic ();
 			}
 		}
 
-		if (_instance == null)
-		{
+		if (_instance == null) {
 			//If I am the first instance, make me the Singleton
 			_instance = this;
-			DontDestroyOnLoad(this);
-		}
-		else
-		{
+			DontDestroyOnLoad (this);
+		} else {
 			//If a Singleton already exists and you find
 			//another reference in scene, destroy it!
-			if (this != _instance)
-			{
-				Destroy(this.gameObject);
+			if (this != _instance) {
+				Destroy (this.gameObject);
 			}
 		}
 
 	}
 
 	// Update is called once per frame
-	void Update()
+	void Update ()
 	{
 
-		if (Input.GetKey("escape"))
-		{
-			Application.LoadLevel("MainMenu");
+		if (Input.GetKey ("escape")) {
+			Application.LoadLevel ("MainMenu");
 
 			// remove singleton
-			Destroy(_instance.gameObject);
+			Destroy (_instance.gameObject);
 		}
 
-		PlayMusic();
+		PlayMusic ();
 	
 	}
 
-	void PlayMusic()
+	void PlayMusic ()
 	{
 
-		if (musicSource != null)
-		{
-			if (musicSource.isPlaying == false && musicSource.enabled == true)
-			{
-				if (musicClips == null)
-				{
+		if (musicSource != null) {
+			if (musicSource.isPlaying == false && musicSource.enabled == true) {
+				if (musicClips == null) {
 					// load music clips
-					musicClips = Resources.LoadAll<AudioClip>("Music/JJR2");
+					musicClips = Resources.LoadAll<AudioClip> ("Music/JJR2");
 				}
-
-				// play random track		
-				int max = musicClips.Length;
-				int randomIndex = Random.Range(0, max);
-				musicSource.clip = musicClips[randomIndex];
-				musicSource.Play();
+				if (musicClips != null && musicClips.Length > 0) {
+					// play random track		
+					int max = musicClips.Length;
+					int randomIndex = Random.Range (0, max);
+					musicSource.clip = musicClips [randomIndex];
+					musicSource.Play ();
+				}
 			}
 		}
 
