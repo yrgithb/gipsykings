7b6bd0eec1db37282abd53e519835ae7fe67a447
Alexander Popov <lxndrppv@gmail.com>
2016-12-13 19:37:07 +0200

    Added GameManager prefab. Added MouseVisibility script. Mouse cursor now invisible on all scenes except main menu.

---
+++ GipsyKings/Assets/Scripts/MouseVisibility.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseVisibility : MonoBehaviour {
+
+    public bool visible = false;
+
+	void Start () {
+        Cursor.visible = visible;
+    }
+	
+}
